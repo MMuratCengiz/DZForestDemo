c81c6a0120b16ae1efdddfc35fcb71c133b05839
Murat Cengiz <cengizm9@gmail.com>
2025-05-13 22:47:44 +0200

    Adding simple import logic now, will go back and fix it soon
    will also fix the api as it is kind of annoying to use

--- ImportAssets/Program.cs
+++ ImportAssets/Program.cs
@@ -2,6 +2,13 @@
 
 using DenOfIz;
 
+var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+InteropString targetDirectory = new("C:/Workspace/DZForestDemo/DZForestDemo/Assets/MeadowForest/");
+InteropString importRoot =
+    new($"{home}/Downloads/Polygon_NatureBiomes_MeadowForest_SourceFiles_v2/Meadow_Source_Files/");
+var texRoot = importRoot.Append("Textures/");
+var fbxRoot = importRoot.Append("FBX/");
+
 EngineDesc engineDesc = new();
 engineDesc.FS.AssetPath = new InteropString("C:/Workspace/DZForestDemo/DZForestDemo/Assets");
 DenOfIzGraphicsInitializer.Initialize(engineDesc);
@@ -9,3 +16,15 @@ DenOfIzGraphicsInitializer.Initialize(engineDesc);
 AssimpImporterDesc importerDesc = new();
 AssimpImporter importer = new(importerDesc);
 
+ImportJobDesc importJobDesc = new();
+importJobDesc.SourceFilePath = fbxRoot.Append("SM_Env_Tree_Meadow_01.fbx");
+importJobDesc.AssetNamePrefix = new InteropString("");
+importJobDesc.TargetDirectory = targetDirectory.Append("Trees/Meadow/");
+
+importJobDesc.Desc = new AssimpImportDesc();
+
+var result = importer.Import(importJobDesc);
+if (result.ResultCode != ImporterResultCode.Success)
+{
+    Console.WriteLine($"Error {result.ResultCode}: {result.ErrorMessage.Get()}");
+}
