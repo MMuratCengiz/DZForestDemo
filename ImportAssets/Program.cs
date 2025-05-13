7352ab0abf6b540a498d4d612481abef6c21291b
Murat Cengiz <cengizm9@gmail.com>
2025-05-13 22:31:24 +0200

    Nothing ready yet but project structure

---
+++ ImportAssets/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DenOfIz;
+
+EngineDesc engineDesc = new();
+engineDesc.FS.AssetPath = new InteropString("C:/Workspace/DZForestDemo/DZForestDemo/Assets");
+DenOfIzGraphicsInitializer.Initialize(engineDesc);
+
+AssimpImporterDesc importerDesc = new();
+AssimpImporter importer = new(importerDesc);
+
