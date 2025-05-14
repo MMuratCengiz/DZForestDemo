bf5043753898135c03d0765ec9f512ff85cfeec2
MuratCengiz <cengizm9@gmail.com>
2025-05-14 23:47:03 +0200

    Slowly setting up asset system, a bit all over the place

--- DZForestDemo/Program.cs
+++ DZForestDemo/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+GraphicsApi graphicsApi = new GraphicsApi(new APIPreference());
+ILogicalDevice logicalDevice = graphicsApi.CreateAndLoadOptimalLogicalDevice();
