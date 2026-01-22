1f6398b3a802721722603ca3d4979ed0577f3bd3
Murat Cengiz <cengizm9@gmail.com>
2026-01-22 23:25:39 +0100

    Cleanup legacy render graph

--- NiziKit/Graphics/Graph/PresentPass.cs
+++
@@ -1,8 +0,0 @@
-using DenOfIz;
-
-namespace NiziKit.Graphics.Graph;
-
-public abstract class PresentPass() : RenderPassBase()
-{
-    public abstract void Execute(ref RenderPassContext ctx, Texture swapChainImage);
-}
--- NiziKit/Graphics/Graph/RenderPass.cs
+++
@@ -1,6 +0,0 @@
-namespace NiziKit.Graphics.Graph;
-
-public abstract class RenderPass() : RenderPassBase()
-{
-    public abstract void Execute(ref RenderPassContext ctx);
-}
