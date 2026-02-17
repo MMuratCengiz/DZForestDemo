78d6271c48da45b6cb8c26a5796fa0d857cbcd65
Murat Cengiz <cengizm9@gmail.com>
2026-02-17 20:49:11 +0100

    Add new scene for animations

--- DZForestDemo.Editor/Program.cs
+++ DZForestDemo.Editor/Program.cs
@@ -3,6 +3,5 @@ using NiziKit.Editor;
 Editor.Run(new EditorDesc
 {
     GameProjectDir = "../DZForestDemo",
-    InitialScene = "Scenes/VikingShowcase.niziscene.json"
-    // InitialScene = "Scenes/CombatDemo.niziscene.json"
+    InitialScene = "Scenes/CombatDemo.niziscene.json"
 });
